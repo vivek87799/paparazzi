5ce357f46405ff45e6362b989cebf090967a7e59
FINken Lab <christoph.steup@ovgu.de>
2016-01-15 01:02:38 +0100

    FINken3 Wall Avoid slowdown for multi copter

--- sw/airborne/modules/finken_model/finken_model_sensors.c
+++ sw/airborne/modules/finken_model/finken_model_sensors.c
@@ -55,7 +55,7 @@ void finken_sensor_model_init(void)
 void finken_sensor_model_periodic(void)
 {
 	static unsigned int filterCount = 0;
-	if(filterCount++%3==0)
+	if(filterCount++%6==0)
 		finken_sonar_filter_periodic();
   // current timestamp
   uint32_t now_ms = get_sys_time_msec();
