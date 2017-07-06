4866182b0d462abe7381747d412d56bff42e7d90
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-07-06 16:46:59 +0200

    Switched vrepSim to iostreams and archives

--- sw/simulator/nps/nps_fdm_vrep.cpp
+++ sw/simulator/nps/nps_fdm_vrep.cpp
@@ -5,6 +5,12 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/asio.hpp>
 
+#include <fstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <thread>
+#include <chrono>
 
 using std::endl;
 using std::ostream;
@@ -14,9 +20,50 @@ using boost::asio::ip::tcp;
 
 struct NpsFdm fdm;
 struct LtpDef_d ltpRef;
-boost::asio::io_service io_service;
 
 ofstream vrepLog(current_path()/"vrep.log");
+
+class VRepClient {
+  private:
+    tcp::iostream s;
+    bool connected=false;
+    void connect() {
+        while(!connected)
+          try
+          {
+            s.connect("localhost", "50013");
+            connected=true;
+          }
+          catch (std::exception& e)
+          {
+            vrepLog << "Exception: " << e.what() << "\n";
+            std::this_thread::sleep_for(std::chrono::seconds(60));
+          }
+    }
+  public:
+      void update() {
+        connect();
+        static unsigned int seqNr = 0;
+        try {
+          {
+            boost::archive::text_oarchive out(s);
+            out << seqNr;
+          }
+          vrepLog << "Query is: " << seqNr++ << std::endl;
+
+          boost::archive::text_iarchive in(s);
+          unsigned int i;
+          in >> i;
+          vrepLog << "Reply is: " << i << std::endl;
+
+        }catch(const std::exception& e) {
+            vrepLog << "Exception: " << e.what() << "\n";
+            vrepLog << "Error: " << s.error().message() << std::endl;
+        }
+      }
+};
+
+VRepClient client;
 
 void nps_fdm_init(double dt) {
 
@@ -40,17 +87,6 @@ void nps_fdm_init(double dt) {
   ltpRef.lla.lon=0.0;
   ltpRef.lla.alt=0.0;
   ltp_def_from_ecef_d(&ltpRef, &ltpRef.ecef);
-  /*
-  ltpRef.ltp_of_ecef[0]=1.0;
-  ltpRef.ltp_of_ecef[1]=0.0;
-  ltpRef.ltp_of_ecef[2]=0.0;
-  ltpRef.ltp_of_ecef[3]=0.0;
-  ltpRef.ltp_of_ecef[4]=1.0;
-  ltpRef.ltp_of_ecef[5]=0.0;
-  ltpRef.ltp_of_ecef[6]=0.0;
-  ltpRef.ltp_of_ecef[7]=0.0;
-  ltpRef.ltp_of_ecef[8]=1.0;
-  */
   fdm.time=0;
   fdm.init_dt=dt;
   vrepLog << "[" << fdm.time << "] vrep fdm init: dt=" << dt << endl;
@@ -58,50 +94,12 @@ void nps_fdm_init(double dt) {
 
 void nps_fdm_run_step(bool_t launch, double *commands, int commands_nb) {
   fdm.time+=fdm.init_dt;
-  fdm.ltp_ecef_vel;
   vrepLog << "[" << fdm.time << "] vrep fdm step: launch=" << (launch?"yes":"no") << " commands=[";
   for(int i=0;i<commands_nb;i++)
     vrepLog << commands[i] << ((i==commands_nb-1)?"":", ");
   vrepLog << "]" << endl;
 
-
-
-  //Server communication
-  try
-  {
-
-    tcp::socket s(io_service);
-    tcp::resolver resolver(io_service);
-    boost::asio::connect(s, resolver.resolve({"localhost", "50013"}));
-
-    char request[1024] = "grabbing data";
-    size_t request_length = std::strlen(request);
-    std::cout << "Query is: " << request << std::endl;
-    boost::asio::write(s, boost::asio::buffer(request, request_length));
-
-    char reply[1024];
-    size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, request_length));
-    std::cout << "Reply is: ";
-    std::cout.write(reply, reply_length);
-    std::cout << "\n";
-    std::cout << "\n";
-
-    char request2[1024] = "sending data";
-    request_length = std::strlen(request);
-    std::cout << "Query is: " << request2 << std::endl;
-    boost::asio::write(s, boost::asio::buffer(request2, request_length));
-    reply_length = boost::asio::read(s, boost::asio::buffer(reply, request_length));
-    std::cout << "Reply is: ";
-    std::cout.write(reply, reply_length);
-    std::cout << "\n";
-
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-
+  client.update();
 }
 
 void nps_fdm_set_wind(double speed, double dir) {
